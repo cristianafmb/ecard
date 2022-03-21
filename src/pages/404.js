ef7a94850044cf948f5ec889543631435a5459ff
cristianafmb <cbaiorte@gmail.com>
2022-03-21 10:46:52 +0000

    page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,136 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import TopMobile from "../components/mobile/top"
+import SocialIconsMobile from "../components/mobile/socialicons"
+import SocialIcons from "../components/desktop/socialicons"
+import InteractMobile from "../components/mobile/interact"
+import Interact from "../components/desktop/interact"
+import Middle from "../components/desktop/middle"
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => {
+  const breakpoints = useBreakpoint();
+
+  const imagemobile = getImage(data.globalJson.bgmiddlemobile)
+  return(
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+      <Seo title="Nuno Carvalho" />
+      <div className="fullscreen">
+        {breakpoints.mobile ? (
+          <>
 
+            <TopMobile data={data.globalJson.top} />
+
+            <div style={{ height: "50vh" }}>
+              <a className="no-text-decoration" href={data.globalJson.top.site} target="_blank">
+                <GatsbyImage image={imagemobile}
+                  quality={100}
+                  formats={["auto", "webp", "avif"]}
+                  alt={data.globalJson.bgmiddlealt}
+                  objectFit="cover"
+                  style={{ height: "50vh" }}
+                />
+              </a>
+
+            </div>
+
+            <SocialIconsMobile data={data.globalJson.social} />
+
+            <InteractMobile data={data.globalJson.interact} />
+
+          </>
+        ) : (<>
+          <div className="max-width fullscreen">
+            <div className="sides max-height d-inline-flex" >
+
+              <SocialIcons data={data.globalJson.social} breakpoints={breakpoints}/>
+
+            </div>
+            <div className="middle d-inline-flex  max-height ">
+
+              <Middle data={data.globalJson.top} bg={data.globalJson.bgmiddle} alt={data.globalJson.bgmiddlealt} />
+
+            </div>
+            <div className="sides d-inline-flex ">
+
+              <Interact data={data.globalJson.interact} />
+
+            </div>
+
+          </div>
+
+        </>)}
+      </div>
+
+    </Layout>
+)
+        }
 export default NotFoundPage
+export const IndexQuery = graphql`
+  query notfoundJson {
+    globalJson {
+      top{
+        img{
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        imgdesktop{
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        alt
+        site
+        whatsappicon{
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        whatsappalt
+        number
+        name
+        company
+        job
+      }
+      bgmiddlealt
+      bgmiddlemobile{
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+      bgmiddle{
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+      social{
+        icon{
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        iconbig{
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        alt
+        link
+      }
+      interact{
+        icon{
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        alt
+        text
+      }
+    }
+  }
+`
